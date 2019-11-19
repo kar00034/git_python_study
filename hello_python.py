59ccb2d8f80dafa6019e7a91fbf365d3b159f6ad
jh1 <kar00034@naver.com>
2019-11-19 10:57:35 +0900

    add() 함수 추가

---
+++ hello_python.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print("hello python")
+    print("hi pycharm")
+    a = 10
+    b = 5
+    resul = a + b
+    print("add({}, {}) => {}".format(a, b, resul))
