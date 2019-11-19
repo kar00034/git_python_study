72280b0e9f4365f07cd6ff52efac3c16d857207f
jh1 <kar00034@naver.com>
2019-11-19 12:33:14 +0900

    파이썬 수업 56~57 tuple 수업 예제 및 bubble sort 추가

---
+++ chapter_git/hello_python.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+if __name__ == "__main__":
+    print("{} {} ".format("hello python", "hi pycharm"))
+    a = 10
+    b = 5
+    print("add({}, {}) = {}".format(a, b, add(a, b)))
+    print("mul({}, {}) = {}".format(a, b, mul(a, b)))
+    print("div({}, {}) = {}".format(a, b, div(a, b)))
