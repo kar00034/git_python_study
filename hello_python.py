59f38306543371b70278cbe9c9c24af7cad2ce4f
jh1 <kar00034@naver.com>
2019-11-19 11:02:03 +0900

    div() 함수 추가, main print문 변경

--- hello_python.py
+++ hello_python.py
@@ -2,10 +2,13 @@ def add(a, b):
     return a + b
 
 
+def div(a, b):
+    return a / b
+
+
 if __name__ == "__main__":
     print("hello python")
     print("hi pycharm")
     a = 10
     b = 5
-    resul = a + b
-    print("add({}, {}) => {}".format(a, b, resul))
+    print("add({}, {}) => {}\nmul({}, {}) = {}".format(a, b, add(a, b),a, b, div(a, b)))
