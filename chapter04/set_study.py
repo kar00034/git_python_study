89c8f3822e33b8e7591175786e1260bdb34c1020
jh1 <kar00034@naver.com>
2019-11-20 15:53:43 +0900

    로또 작성 완료 보너스 중복 제거 완료

--- chapter04/set_study.py
+++ chapter04/set_study.py
@@ -7,14 +7,48 @@ from chapter04.exam02 import bubble_sort
 def lotto_generator():
     lotto_num = set()
     while len(lotto_num) < 6:
-        lotto_num.add(rnd.randint(1, 46))
+        lotto_num.add(rnd.randint(1, 45))
     return lotto_num
 
 
-if __name__ == "__main__":
-    rnd.seed()
-
+def lotto_start(a, b, c, d, e, f):
     sorted_lotto = bubble_sort(list(lotto_generator()))
     # sorted_lotto.append(rnd.randint(1,46))
+    bonus = rnd.randint(1, 45)
+    print("수정 전 보너스번호 : {}".format(bonus))
+    while sorted_lotto.count(bonus) == 1:
+        bonus = rnd.randint(1, 45)
+    print("로또 번호 : {}".format(sorted_lotto))  # 정렬된 결과
+    print("최종 보너스 번호 : {}".format(bonus))
 
-    print("로또 번호 : {}".format(sorted_lotto)) # 정렬된 결과
+    mylist = {a, b, c, d, e, f}
+    cnt = 0
+    bon = 0
+    for i in sorted_lotto:
+        for j in mylist:
+            if j == i:
+                cnt = cnt + 1
+    for i in mylist:
+        if bonus == i:
+            bon = 1
+
+    if cnt == 6:
+        print("1등입니다 축하합니다")
+    elif cnt == 5:
+        if bon == 1:
+            print("2등입니다.")
+        else:
+            print("3등입니다.")
+    elif cnt == 4:
+        print("4등입니다.")
+    elif cnt == 3:
+        print("5등입니다.")
+    else:
+        print("꽝입니다.")
+    print("{} {}".format(cnt,bon))
+
+
+if __name__ == "__main__":
+    # rnd.seed(1)
+    lotto_start(5,8,9,17,32,29)
+
