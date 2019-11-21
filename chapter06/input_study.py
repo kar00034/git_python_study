6d3f8e0d1adc61a47733dcd64e91dcb1ae31101c
jh1 <kar00034@naver.com>
2019-11-21 10:19:53 +0900

    input_study.py 둘레 계산 추가

--- chapter06/input_study.py
+++ chapter06/input_study.py
@@ -1,3 +1,5 @@
-a = input("정사각형의 한변의 길이는?")
-area = int(a) ** 2
+b = input("정사각형의 한변의 길이는?")
+area = int(b) ** 2
+rec_round = b * 4
 print("정사각형의 넓이 : {}".format(area))
+print("정사각형의 둘레 : {}".format(rec_round))
