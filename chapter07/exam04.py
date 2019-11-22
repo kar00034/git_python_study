93ccaa33376d8994828cb81624a519366331af78
jh1 <kar00034@naver.com>
2019-11-22 10:50:58 +0900

    exam list 완성

--- chapter07/exam04.py
+++ chapter07/exam04.py
@@ -7,20 +7,25 @@
 # 5)프린트하여 제출
 # 6)이왕이면 2개(dictionary + 재량)
 
-# 학생 정보 : 학번, 학생명, 국어, 영어, 수학, 총점, 평균, 등수
+# 학생 정보 : 학번, 학생명, 국어, 영어, 수학, 총점, 평균
 file_path = "student.txt"
 menu = 0
-# if __name__ == "__main__":
-#     stu_list = (("01", "홍길동", "90", "90", "90"))
-#     total = 0
-#     for i in range(0,len(stu_list)):
-#         for j in range(2,5):
-#             total = total + int(stu_list[i][j])
-#     for i in range(0,len(stu_list)):
-#         student_dic = {"1": {"학번": "01", "이름": "홍길동", "국어": stu_list[i][2], "영어": stu_list[i][3], "수학": stu_list[i][4], "총점": total,
-#               "평균": total / 3}}
-#         print(student_dic)
-#     # student_management_dic(student_dic)
+slist = []
+
+
+def read_file():
+    global slist
+    try:
+        f = open("student.txt", "r")
+        line = f.readline()
+        while line:
+            line_list = line.split()
+            slist.append(line_list)
+            line = f.readline()
+    except FileNotFoundError as er:
+        print("파일이 없습니다.", e, sep='\n')
+    finally:
+        f.close()
 
 
 def printmenu():
@@ -43,111 +48,103 @@ def printmenu():
     return menu
 
 
-def addstu():
-    global file_path
-    err = 0
-    stu = ["stu_number", "name", "kor", "eng", "math"]
-    print("다음 정보를 입력해주세요.")
-
-    f = open(file_path, "a")
-    r = open(file_path, "r")
-
-    for i in range(0, len(stu)):
-        stu[i] = input("{} : ".format(stu[i]))
-    for i in r:
-        if stu[0] == i[0:1]:
-            print("중복입니다")
-            err = 1
-
-    if err == 0:
-        f.write('\t\t'.join(stu))
-        f.write('\n')
-    else:
-        print("초기 화면으로 돌아갑니다")
-
-    r.close()
-    f.close()
-
-
 def stulist():
-    print("이름\t\t학생번호\tkor\t\teng\t\tmath")
-    with open(file_path, "r") as f:
-        for info in f:
-            print("{}".format(info), end='')
+    global slist
+    print("이름\t학생번호\tkor\teng\tmath\t총점\t\t평균")
+    for i in range(len(slist)):
+        total = int(slist[i][2]) + int(slist[i][3]) + int(slist[i][4])
+        avg = total / 3
+        print("{}\t{}\t\t{}\t{}\t{}\t\t{}\t\t{:.2f}".format(slist[i][0], slist[i][1], slist[i][2], slist[i][3],
+                                                            slist[i][4], total, avg))
 
 
-def delstu():
+def addstu():
     cnt = 0
-    na = input("삭제할 학생의 번호를 입력하세요 > ")
-    r = open(file_path, "r")
-    for i in r:
-        if na == i[0:1]:
-            cnt = 1
+    global slist
+    add_list = ["학번", "이름", "kor", "eng", "math"]
+    print("다음 정보를 입력하세요.")
+    for i in range(0, len(add_list)):
+        add_list[i] = input("{} : ".format(add_list[i]))
+    for i in range(len(slist)):
+        if add_list[0] == slist[i][0]:
+            print("중복입니다. 다시 입력해주세요")
             print(i)
-            print(na)
-
-    if cnt == 1:
-        ans = input("삭제 하실거면 1을 눌러주세요")
-    if ans == 1:
-        f = open(file_path, "a")
-        for i in f:
-            if na == f[0:1]:
-                pass
-
-
-    f.close()
-    r.close()
+            cnt = 1
+            break
+    if cnt != 1:
+        slist.append(add_list)
+        print("입력되었습니다.")
 
 
 def editstu():
-    cnt = 0
-    na = input("수정할 학생의 번호를 입력하세요 > ")
-    r = open(file_path, "r")
-    f = open(file_path, "a+")
-    for i in r:
-        if na == i[0:1]:
-            cnt = 1
-
+    global slist
+    cnt = 1
+    print("학생번호\t이름\t\tkor\t\teng\t\tmath")
+    for i in range(len(slist)):
+        for j in range(len(slist[i])):
+            print(slist[i][j],end='\t\t')
+        print()
+    tar = input("수정할 학생의 번호를 입력하세요 > ")
+    for i in range(len((slist))):
+        if tar == slist[i][0]:
+            edit_list = ["이름", "kor", "eng", "math"]
+            print("다음 정보를 입력하세요.")
+            for j in range(0, len(edit_list)):
+                slist[i][j+1] = input("{} : ".format(edit_list[j]))
+                cnt = 0
+            break
     if cnt == 1:
-        stu = ["stu_number", "name", "kor", "eng", "math"]
-        print("다음 정보를 입력해주세요.")
-        for i in range(0, len(stu)):
-            stu[i] = input("{} : ".format(stu[i]))
-
-        for i in r:
-            if stu[0] != i[0:1]:
-                f.write('\t\t'.join(stu))
-                print("수정되었습니다.")
-            else:
-                print("중복입니다")
-    r.close()
-    f.close()
+        print("해당학생은 없습니다.")
 
 
+def delstu():
+    global slist
+    cnt = 0
+    print("학생번호\t이름\t\tkor\t\teng\t\tmath")
+    for i in range(len(slist)):
+       for j in range(len(slist[i])):
+           print(slist[i][j], end='\t\t')
+       print()
+    tar = input("삭제할 학생의 번호를 입력하세요 > ")
+    for i in range(len(slist)):
+       if tar == slist[i][0]:
+           ans = input("삭제하려면 y을 눌러주세요")
+           if ans == 'y':
+               del slist[i]
+               print("삭제되었습니다.")
+               break
+           else:
+               print("취소되었습니다.")
+               break
+           cnt = 0
+    if cnt != 1:
+        print("해당 학생은 없습니다.")
 
+
+
+def save():
+    global slist
+    with open(file_path, "w")as f:
+        for i in range(len((slist))):
+            f.write(slist[i][0] + '\t' + slist[i][1] + '\t' + slist[i][2] + '\t' +
+                    slist[i][3] + '\t' + slist[i][4] + '\n')
+    print("프로그램을 종료합니다.")
 
 
 if __name__ == "__main__":
+    read_file()
     while True:
-        printmenu() #완
+        printmenu()
         if menu == 1:
-            stulist() # 완
+            stulist()
         elif menu == 2:
-            addstu() # 완
+            addstu()
         elif menu == 3:
             editstu()
         elif menu == 4:
             delstu()
         elif menu == 5:
-            print("프로그램을 종료합니다.")
+            save()
             break
         else:
             print("다시 선택해주세요.")
-
-# 구상
-# list(name1,name2,name3)
-# list2(no1,no2,no3)
-#
-# dic{name1:{},name2:{},name3:{}}
-z= input("")
-print(z)
